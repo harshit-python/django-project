9434cc0b1ba9f6fc4513591bd4b2387ef390f265
harsh <hsaxena1998@gmail.com>
2023-07-22 00:44:47 +0530

    user authentication

--- users/views.py
+++ users/views.py
@@ -1,7 +1,61 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Profile
+from django.contrib.auth.models import User
+from django.contrib.auth import login, authenticate, logout
+from django.contrib import messages
+from .forms import CustomUserCreationForm
+
 
 # Create your views here.
+
+def loginUser(request):
+    page = "login"
+    if request.user.is_authenticated:
+        return redirect("profiles")
+
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+        try:
+            user = User.objects.get(username=username)
+        except:
+            messages.error(request, "Username does not exists")
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect("profiles")
+        else:
+            messages.error(request, "Username or Password is incorrect")
+
+    return render(request, "users/login_register.html")
+
+
+def logoutUser(request):
+    logout(request)
+    messages.info(request, "User was successfully logged out!")
+    return redirect("login")
+
+
+def registerUser(request):
+    page = "register"
+    form = CustomUserCreationForm()
+
+    if request.method == "POST":
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.username = user.username.lower()
+            user.save()
+
+            messages.success(request, "User account was created!")
+            login(request, user)
+            return redirect("profiles")
+        else:
+            messages.error(request, "Error in creating user")
+
+    context = {"page": page, "form": form}
+    return render(request, "users/login_register.html", context)
+
 
 def profiles(request):
     profiles = Profile.objects.all()
